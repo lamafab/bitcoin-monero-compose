dfd05bbe8d61f1b8b9c094198c6e59c4c87b1c87
lamafab <42901763+lamafab@users.noreply.github.com>
2022-10-21 21:57:42 +0200

    update bash.sh of gen-vpn-keys, add function

--- services/_gen-vpn-keys/setup.sh
+++ services/_gen-vpn-keys/setup.sh
@@ -1,24 +1,37 @@
-umask 077
+# Set permissions of created files for root-only
+#umask 300
+umask 000
 
-SERVER_SECKEY=$(wg genkey)
-SERVER_PUBKEY=$(echo $SERVER_SECKEY | wg pubkey)
+gen_keys () {
+	if [[ -f /data/$1 ]]; then
+		SECKEY=$(cat "/data/$1")
+		PUBKEY=$(echo $SECKEY | wg pubkey)
+		echo $SECKEY $PUBKEY
+	else
+		SECKEY=$(wg genkey)
+		PUBKEY=$(echo $SECKEY | wg pubkey)
+		echo $SECKEY $PUBKEY
+	fi
+}
 
-BITCOIN_SECKEY=$(wg genkey)
-BITCOIN_PUBKEY=$(echo $BITCOIN_SECKEY | wg pubkey)
+cp wireguard.conf wg0.conf
 
-LIGHTNING_SECKEY=$(wg genkey)
-LIGHTNING_PUBKEY=$(echo $LIGHTNING_SECKEY | wg pubkey)
+read SERVER_SECKEY SERVER_PUBKEY < <(gen_keys "vpn-server_private.key")
 
-MONERO_SECKEY=$(wg genkey)
-MONERO_PUBKEY=$(echo $MONERO_SECKEY | wg pubkey)
+read BITCOIN_SECKEY BITCOIN_PUBKEY < <(gen_keys "bitcoin_private.key")
 
-sed -i 's/<VPN_SERVER_SECKEY>/${VPN_SERVER_SECKEY}/g' wireguard.conf
-sed -i 's/<BITCOIN_PUBKEY>/${BITCOIN_PUBKEY}/g' wireguard.conf
-sed -i 's/<LIGHTNING_PUBKEY>/${LIGHTNING_PUBKEY}/g' wireguard.conf
-sed -i 's/<MONERO_PUBKEY>/${MONERO_PUBKEY}/g' wireguard.conf
+read LIGHTNING_SECKEY LIGHTNING_PUBKEY < <(gen_keys "lightning_private.key")
 
-cp wireguard.conf /data
+read MONERO_SECKEY MONERO_PUBKEY < <(gen_keys "monero_private.key")
 
+sed -i "s|<VPN_SERVER_SECKEY>|'$SERVER_SECKEY'|" wg0.conf
+sed -i "s|<BITCOIN_PUBKEY>|'$BITCOIN_PUBKEY'|" wg0.conf
+sed -i "s|<LIGHTNING_PUBKEY>|'$LIGHTNING_PUBKEY'|" wg0.conf
+sed -i "s|<MONERO_PUBKEY>|'$MONERO_PUBKEY'|" wg0.conf
+
+cp wg0.conf /data
+
+echo $SERVER_SECKEY > /data/vpn-server_private.key
 echo $SERVER_PUBKEY > /data/vpn-server_public.key
 echo $BITCOIN_SECKEY > /data/bitcoin_private.key
 echo $LIGHTNING_SECKEY > /data/lightning_private.key
