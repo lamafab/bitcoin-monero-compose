5cb3fc559d286f76dcd9c4bf66379809ad5f8572
lamafab <42901763+lamafab@users.noreply.github.com>
2022-10-23 19:10:09 +0200

    update subnets and setup script

--- services/bitcoin/setup.sh
+++ services/bitcoin/setup.sh
@@ -1,7 +1,8 @@
 if "${INSTALL_VPN}" == "true"; then
+	echo ">> VPN enabled, setting up routes..."
 	apt install -y iproute2
 	ip route del default
-	ip route add default via 10.0.0.1
+	ip route add default via 10.66.0.1
 fi
 
 echo ">> IP ROUTE:"
@@ -10,10 +11,13 @@ ip route show
 echo ">> IP ADDRESS:"
 ip addr
 
-#update-ca-certificates
+update-ca-certificates
 
 # Download and verify bitcoin package
-#wget ${LINK} -O bitcoin.tar.gz
-#sha256sum -c checksum.txt
-#mkdir bitcoin
-#tar xvzf bitcoin.tar.gz -C bitcoin --strip-components=1
+wget ${LINK} -O bitcoin.tar.gz
+sha256sum -c checksum.txt
+mkdir bitcoin
+tar xvzf bitcoin.tar.gz -C bitcoin --strip-components=1
+
+# Add bitcoin user
+adduser --disabled-password --gecos "" bitcoin
