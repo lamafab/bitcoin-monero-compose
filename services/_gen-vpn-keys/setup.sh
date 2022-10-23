a6828c2e0549d65683491b5c4551bd8fc9d4b747
lamafab <42901763+lamafab@users.noreply.github.com>
2022-10-23 02:15:02 +0200

    implement VPN server specific logic

--- services/_gen-vpn-keys/setup.sh
+++ services/_gen-vpn-keys/setup.sh
@@ -1,6 +1,7 @@
 # Set permissions of created files for root-only.
 umask 077
 
+# Generates a new client keypair, if not present yet.
 gen_keys () {
 	if [ ! -e /data/wg_${1}_client.conf ]; then
 		echo "GENERATING new keypair for ${1}"
@@ -13,6 +14,7 @@ gen_keys () {
 
 		# Update server config
 		sed -i "s|<${1^^}_PUBKEY>|'$PUB_KEY'|" wg_vpn_server.conf
+	# ... otherwise skip.
 	else
 		echo "FOUND existing wg_${1}_client.conf, skipping..."
 	fi
@@ -21,10 +23,16 @@ gen_keys () {
 cp wireguard_server.conf wg_vpn_server.conf
 cp wireguard_client.conf client.conf
 
-VPN_SK=$(wg genkey) && VPN_PK=$(echo $VPN_SK | wg pubkey)
+# Generates the VPN server keypair, if not present yet.
+if [ ! -e /data/wg_vpn_server.conf ]; then
+	echo "GENERATING new VPN server keypair"
+	VPN_SK=$(wg genkey) && VPN_PK=$(echo $VPN_SK | wg pubkey)
 
-sed -i "s|<VPN_SERVER_SECKEY>|'$VPN_SK'|" wg_vpn_server.conf
-sed -i "s|<VPN_SERVER_PUBKEY>|'$VPN_PK'|" client.conf
+	sed -i "s|<VPN_SERVER_SECKEY>|'$VPN_SK'|" wg_vpn_server.conf
+	sed -i "s|<VPN_SERVER_PUBKEY>|'$VPN_PK'|" client.conf
+else
+	echo "FOUND existing VPN server config, skipping..."
+fi
 
 gen_keys "bitcoin"	10.1.0.2
 gen_keys "lightning" 10.2.0.2
