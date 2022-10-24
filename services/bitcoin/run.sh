5c3a4d17541e305fd9e2f0e0f83d0f14ace8d1a6
lamafab <42901763+lamafab@users.noreply.github.com>
2022-10-24 20:35:21 +0200

    set additional parameters via compose, cleanup run.sh

--- services/bitcoin/run.sh
+++ services/bitcoin/run.sh
@@ -1,27 +1,37 @@
-chown -R bitcoin:bitcoin /data
+echo '
+⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⣾⣿⣿⣿⣿⣷⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀
+⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⠟⠿⠿⡿⠀⢰⣿⠁⢈⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀
+⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣤⣄⠀⠀⠀⠈⠉⠀⠸⠿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀
+⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⢠⣶⣶⣤⡀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⡆
+⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠼⣿⣿⡿⠃⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣷
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢀⣀⣀⠀⠀⠀⠀⢴⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⢿⣿⣿⣿⣿⣿⣿⣿⢿⣿⠁⠀⠀⣼⣿⣿⣿⣦⠀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⡿
+⠸⣿⣿⣿⣿⣿⣿⣏⠀⠀⠀⠀⠀⠛⠛⠿⠟⠋⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⠇
+⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⣤⡄⠀⣀⣀⣀⣀⣠⣾⣿⣿⣿⣿⣿⣿⣿⡟⠀
+⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣄⣰⣿⠁⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀
+⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀
+⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⢿⣿⣿⣿⣿⡿⠿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀'
 
 # TODO: setup DNS over VPN
 if "${INSTALL_VPN}" == "true"; then
-	echo ">> VPN enabled, setting up routes..."
-	apt install -y iproute2
-	# TODO: Delete:
-	apt install -y curl iputils-ping dnsutils python3
-
+	echo -e "\n## VPN enabled, setting up routes..."
 	ip route del default
 	ip route add default via 10.50.0.10
 
-	echo ">> IP ROUTE:"
+	echo -e "\n## IP ROUTE:"
 	ip route show
 
-	echo ">> IP ADDRESS:"
+	echo -e "\n## IP ADDRESS:"
 	ip addr
 
-	echo ">> ping 1.1.1.1"
-	ping 1.1.1.1 -c 3
-
-	echo ">> EXTERNAL IP"
-	echo "$(curl -s ifconfig.me)"
+	echo -e "\n## EXTERNAL IP"
+	echo "You will connect to the network via $(curl -s ifconfig.me)"
 fi
 
-#/app/bitcoin/bin/bitcoind -datadir=/data
-python3 -m http.server 8000
+echo -e "\n## About to start service ..."
+sleep 5
+
+/app/bitcoin/bin/bitcoind -datadir=${DATA_DIR} -port=${P2P_PORT} ${CLI_ARGS}
